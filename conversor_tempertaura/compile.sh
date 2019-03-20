dcadbc036952eac6e47a06c07c4d2d2b51c8a88c
Bignone <eduardobustosmiranda@hotmail.com>
2019-03-20 16:30:06 +0100

    compile using make

--- conversor_tempertaura/compile.sh
+++ conversor_tempertaura/compile.sh
@@ -1,4 +1,10 @@
-echo "Compiling conversor program..."
+echo "* Compiling conversor program *"
+echo "."
+echo "Cleaning previous files..."
+make -f makefile clean
+echo "."
+echo "Compiling..."
 #gcc conversor.c -Wall -o conversor
-make -f makefile conversor
+make
+echo "."
 echo "Finished!"
