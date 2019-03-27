1c810fd4d5781869547fff61b28669710600a276
Bignone <eduardobustosmiranda@hotmail.com>
2019-03-27 15:53:40 +0100

    all right with debug

--- calcular_dia/calcular_dia.c
+++ calcular_dia/calcular_dia.c
@@ -3,7 +3,7 @@
 
 int is_bisiest_year(int year) {
     int is_b = 0;
-    if ( year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ){
+    if ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ){
         is_b = 1;
     }
     return is_b;
@@ -11,7 +11,7 @@ int is_bisiest_year(int year) {
 }
 
 void askDate(int *in_date)  {
-    printf("Insert the date as : \n");
+    printf("Insert the date \n");
 
     printf("Insert day: ");
     scanf("%d", &in_date[0]);
