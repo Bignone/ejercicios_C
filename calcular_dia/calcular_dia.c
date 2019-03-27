056261eeab8b570ecd5699e2eb9db2912853e670
Bignone <eduardobustosmiranda@hotmail.com>
2019-03-27 16:44:37 +0100

    some doc

--- calcular_dia/calcular_dia.c
+++ calcular_dia/calcular_dia.c
@@ -3,7 +3,8 @@
 
 #include "bisiest/bisiest.h"
 
-/*int is_bisiest_year(int year) {
+/* This is included in  "bisiest/bisiest.h"
+int is_bisiest_year(int year) {
     int is_b = 0;
     if ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ){
         is_b = 1;
